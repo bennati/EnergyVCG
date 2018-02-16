431418ae30827882f367034980a713535456bb7f
bennati <st.bennati@gmail.com>
2018-02-16 09:57:34 +0100

    Reorganized data and plots into subdirs

--- src/simulation.py
+++ src/simulation.py
@@ -43,6 +43,11 @@ def subset_df(df,conditions):
     return ret.reset_index()
 
 def run_experiment(test,conf):
+    print("starting "+str(test))
+    if not os.path.exists("plots/"+str(test)):
+        os.makedirs("plots/"+str(test))
+    if not os.path.exists("data/"+str(test)):
+        os.makedirs("data/"+str(test))
     log_tot=[]
     qtables=[]
     qlearning=False
@@ -76,14 +81,14 @@ def run_experiment(test,conf):
     ### prepare tables ###
     stats_gini_contribs=pd.concat([pd.DataFrame(i["decisions"]) for i in log_tot])
     contrib_hist=compute_contrib_hist(stats_gini_contribs,varnames)
-    contrib_hist.to_csv("./data/contrib_hist.csv.gz",index=False,compression='gzip')
+    contrib_hist.to_csv("./data/"+str(test)+"/contrib_hist.csv.gz",index=False,compression='gzip')
     #contrib_hist=pd.read_csv("./data/contrib_hist.csv.gz")
     # aggregate over all agent ids and compute gini coefficients
     stats_gini_contribs=stats_gini_contribs.groupby(varnames+["agentID","repetition"],as_index=False).agg({"contributed":np.sum,"contribution":np.sum}) # sum up all contributions in each simulation (over all timesteps)
     stats_gini_contribs=stats_gini_contribs.groupby(varnames+["repetition"],as_index=False).agg({"contributed":gini,"contribution":gini}) # compute gini coefficient across agents
     stats_gini_contribs=stats_gini_contribs.rename(columns={"contributed":"Contributors","contribution":"Values"})
-    stats_gini_contribs.to_csv("./data/stats_gini_contribs.csv.gz",index=False,compression='gzip')
-    #stats_gini_contribs=pd.read_csv("./data/stats_gini_contribs.csv.gz")
+    stats_gini_contribs.to_csv("./data/"+str(test)+"/stats_gini_contribs.csv.gz",index=False,compression='gzip')
+    #stats_gini_contribs=pd.read_csv("./data/"+str(test)+"/stats_gini_contribs.csv.gz")
     ## compute stats qtables
     if qlearning:
         qtables=pd.concat(qtables)
@@ -92,32 +97,32 @@ def run_experiment(test,conf):
         qtables["state_cost"]=qtables["index"].transform(lambda x: x[1])
         qtables.drop("index",axis=1,inplace=True)
         qtables["prob"]=[boltzmann([r[1]["yes"],r[1]["no"]],0.1)[0] for r in qtables.iterrows()] # normalize qvalues, prob is the probability of contributing using the boltzmann equation
-        qtables.to_csv("./data/qtables.csv.gz",index=False,compression='gzip')
-        #qtables=pd.read_csv("./data/qtables.csv.gz")
+        qtables.to_csv("./data/"+str(test)+"/qtables.csv.gz",index=False,compression='gzip')
+        #qtables=pd.read_csv("./data/"+str(test)+"/qtables.csv.gz")
     ### start with computing stats across all parameter configurations ###
     # compute evaluation over time
     stats_evalt=get_stats(log_tot,"evaluation",idx=["timestep"],cols=["gini","cost","efficiency","social_welfare","success","num_contrib"])
-    stats_evalt.to_csv("./data/stats_evalt.csv.gz",index=False,compression='gzip')
-    #stats_evalt=pd.read_csv("./data/stats_evalt.csv.gz")
-    plot_measures(stats_evalt,"timestep","./plots/"+str(test)+"_eval_"+str("time")+".pdf")
+    stats_evalt.to_csv("./data/"+str(test)+"/stats_evalt.csv.gz",index=False,compression='gzip')
+    #stats_evalt=pd.read_csv("./data/"+str(test)+"/stats_evalt.csv.gz")
+    plot_measures(stats_evalt,"timestep","./plots/"+str(test)+"/eval_"+str("time")+".pdf")
     if qlearning:
         qtables_stats=compute_stats([qtables],idx=["state_val","state_cost"],columns=["prob","num"])
-        plot_trend(qtables_stats,"state_cost","./plots/"+str(test)+"_qtables_cost.pdf",yname="state_val",trends=["prob"])
-        plot_trend(qtables_stats,"state_val","./plots/"+str(test)+"_qtables_val.pdf",yname="state_cost",trends=["prob"])
+        plot_trend(qtables_stats,"state_cost","./plots/"+str(test)+"/qtables_cost.pdf",yname="state_val",trends=["prob"])
+        plot_trend(qtables_stats,"state_val","./plots/"+str(test)+"/qtables_val.pdf",yname="state_cost",trends=["prob"])
     ### now move to computing statistics that aggregate on one of the parameters ###
     for varname in varnames:
         stats_gini=compute_stats([stats_gini_contribs],[varname],columns=["Contributors","Values"]) # average across repetitions
-        plot_trend(stats_gini,varname,"./plots/"+str(test)+"_gini_"+str(varname)+".pdf")
+        plot_trend(stats_gini,varname,"./plots/"+str(test)+"/gini_"+str(varname)+".pdf")
         stats_rew=get_stats(log_tot,"reward",idx=[varname],cols=["reward"])
-        plot_trend(stats_rew,varname,"./plots/"+str(test)+"_rewards_"+str(varname)+".pdf")
+        plot_trend(stats_rew,varname,"./plots/"+str(test)+"/rewards_"+str(varname)+".pdf")
         stats_perc=get_stats(log_tot,"perception",idx=[varname],cols=["value","cost"])
-        plot_trend(stats_perc,varname,"./plots/"+str(test)+"_perceptions_"+str(varname)+".pdf")
+        plot_trend(stats_perc,varname,"./plots/"+str(test)+"/perceptions_"+str(varname)+".pdf")
         stats_decs=get_stats(log_tot,"decisions",idx=[varname],cols=["contribution","cost","contributed"])
-        plot_trend(stats_decs,varname,"./plots/"+str(test)+"_decisions_"+str(varname)+".pdf")
+        plot_trend(stats_decs,varname,"./plots/"+str(test)+"/decisions_"+str(varname)+".pdf")
         stats_eval=get_stats(log_tot,"evaluation",idx=[varname],cols=["gini","cost","efficiency","social_welfare","success","num_contrib"])
-        plot_measures(stats_eval,varname,"./plots/"+str(test)+"_eval"+str(varname)+".pdf")
+        plot_measures(stats_eval,varname,"./plots/"+str(test)+"/eval"+str(varname)+".pdf")
         stats_contrib_hist=compute_stats(contrib_hist,idx=[varname,"value"],columns=["cnt"])
-        plot_trend(stats_contrib_hist,"value","./plots/"+str(test)+"_contrib_hist_"+str(varname)+".pdf",yname=varname)
+        plot_trend(stats_contrib_hist,"value","./plots/"+str(test)+"/contrib_hist_"+str(varname)+".pdf",yname=varname)
 
     ### now compute statistics for each parameter configuration, aggregating only on repetitions ###
     stats_contrib_hist2=compute_stats(contrib_hist,idx=varnames+["value"],columns=["cnt"])
@@ -126,10 +131,11 @@ def run_experiment(test,conf):
         pdesc="_".join([str(k)+str(v) for k,v in dict(p).items()])
         # temporal evolution of measures
         tmp=subset_df(stats_t,p)
-        plot_measures(tmp,"timestep","./plots/"+str(test)+"_time_"+pdesc+".pdf")
+        plot_measures(tmp,"timestep","./plots/"+str(test)+"/time_"+pdesc+".pdf")
         # distribution of contributions
         # tmp=subset_df(stats_contrib_hist2,p)
-        # plot_trend(tmp,"value","./plots/"+str(test)+"_contrib_hist_"+pdesc+".pdf")
+        # plot_trend(tmp,"value","./plots/"+str(test)+"/contrib_hist_"+pdesc+".pdf")
+
     ## compute qtable heatmaps
     if qlearning:
         stats_q=compute_stats([qtables],idx=["state_val","state_cost"]+varnames,columns=["prob","num"])
@@ -138,13 +144,16 @@ def run_experiment(test,conf):
             tmp=subset_df(stats_q,p)
             f=lambda df,col: np.histogram2d(df["state_cost"],df["state_val"],weights=df[col+"_mean"],bins=[np.append(df["state_val"].unique(),[df["state_val"].max()+1]),np.append(df["state_cost"].unique(),[df["state_cost"].max()+1])])
             heatmap_choice,xlabs,ylabs=f(tmp,"prob")
-            plot_hmap(heatmap_choice,"Average qvalue associated to contribution",str(test)+"_heat_q_choice"+pdesc+".pdf","./plots",xlab="Value",ylab="Cost",ticks=[range(len(xlabs[:-1])),range(len(ylabs[:-1]))],ticklabs=[xlabs[:-1],ylabs[:-1]],inverty=False)
-            heatmap_count,xlabs,ylabs=f(tmp,"num")
-            plot_hmap(heatmap_count,"Average number of occurrences of a state",str(test)+"_heat_q_count"+pdesc+".pdf","./plots",xlab="Value",ylab="Cost",ticks=[range(len(xlabs[:-1])),range(len(ylabs[:-1]))],ticklabs=[xlabs[:-1],ylabs[:-1]],inverty=False)
+            plot_hmap(heatmap_choice,"Average qvalue associated to contribution",str(test)+"/heat_q_choice"+pdesc+".pdf","./plots",xlab="Value",ylab="Cost",ticks=[range(len(xlabs[:-1])),range(len(ylabs[:-1]))],ticklabs=[xlabs[:-1],ylabs[:-1]],inverty=False)
+            # heatmap_count,xlabs,ylabs=f(tmp,"num")
+            # plot_hmap(heatmap_count,"Average number of occurrences of a state",str(test)+"/heat_q_count"+pdesc+".pdf","./plots",xlab="Value",ylab="Cost",ticks=[range(len(xlabs[:-1])),range(len(ylabs[:-1]))],ticklabs=[xlabs[:-1],ylabs[:-1]],inverty=False)
             ## compute histograms
             q_exp=subset_df(qtables,p) # subset with current experimental conditions
-            plot_qtable_hist(q_exp,"./plots/"+str(test)+"_qhist_"+pdesc+".pdf","state_val","state_cost","prob",str(dict(p)))
-            plot_qtable_heat(q_exp,"./plots/heatmaps/"+str(test)+"_qheat_"+pdesc,"state_val","state_cost","prob")
+            plot_qtable_hist(q_exp,"./plots/"+str(test)+"/qhist_"+pdesc+".pdf","state_val","state_cost","prob",str(dict(p)))
+            prefix="./plots/"+str(test)+"/heatmaps"
+            if not os.path.exists(prefix):
+                os.makedirs(prefix)
+            plot_qtable_heat(q_exp,prefix+"/qheat_"+pdesc,"state_val","state_cost","prob")
 
 if __name__ == '__main__':
     tests={"qlearn":{"T":1000,"reps":3,"params":{"N":[10,20,30],"n1":[0],"n2":[2,5,8]},"meas_fct":MeasurementGenUniform,"dec_fct_sup":DecisionLogicSupervisorEmpty,"dec_fct":DecisionLogicQlearn,"rew_fct":RewardLogicUniform}}#
