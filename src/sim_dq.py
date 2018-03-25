4f372fd631cac5c0b9d486c86a79fe9630e47e91
bennati <st.bennati@gmail.com>
2018-03-25 19:11:10 +0200

    Added DQL decision logic for the supervisor

--- src/sim_dq.py
+++ src/sim_dq.py
@@ -16,38 +16,52 @@ def renormalize(n, range1, range2):
     delta2 = range2[1] - range2[0]
     return (delta2 * (n - range1[0]) / delta1) + range2[0]
 
-# class MeasurementGenUniformDQ(BaseMeasurementGen):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__()
-#         self.n1=kwargs["n1"]
-#         self.n2=kwargs["n2"]
-#         assert(self.n1>=0)
-#         assert(self.n2>self.n1)
-
-#     def get_measurements(self,population,timestep):
-#         """
-#         Returns a list of dictionaries containing the measurements: the state of each agent at the current timestep
-#         """
-#         vals=[np.random.uniform(max(1,self.n1),self.n2) for _ in population]
-#         costs=[np.random.uniform(max(1,self.n1),self.n2) for _ in population]
-#         # thresh=max(1,int(sum(vals)*np.random.uniform(0,1)))
-#         thresh=len(population) #np.random.randint(1,3)
-#         assert(thresh<=sum(vals))
-#         ret=[{"value":v,"cost":c,"timestep":timestep,"agentID":i,"threshold":thresh} for i,(v,c) in enumerate(zip(vals,costs))]
-#         return ret
-
 class DecisionLogicSupervisorDQ(BaseDecisionLogic):
     """
     Returns a constant decision
     """
-    def get_decision(self,perceptions):
-        decs=[a.get_decision() for a in self.model.schedule.agents]
-        idxs=[a.unique_id for a in self.model.schedule.agents]
-        tmp1=pd.DataFrame(data={"action":decs,"agentID":idxs})
-        tmp=pd.merge(pd.DataFrame(perceptions),tmp1,on=["agentID"])
-        # print(decs)
-        self.act=[{"contribution":(r[1]["value"] if r[1]["action"] else np.nan),"cost":(r[1]["cost"] if r[1]["action"] else np.nan),"privacy":(1 if r[1]["action"] else 0),"agentID":r[1]["agentID"],"contributed":r[1]["action"],"timestep":r[1]["timestep"],"threshold":r[1]["threshold"]} for r in tmp.iterrows()]
-        return self.act
+    def __init__(self,model,alpha=0.01,gamma=0.0,training=True):
+        super().__init__(model)
+        possible_values=list(range(max(1,self.model.measurement_fct.n1),self.model.measurement_fct.n2)) # TODO binarize a continuous range
+        possible_costs=list(range(max(1,self.model.measurement_fct.n1),self.model.measurement_fct.n2)) # TODO binarize a continuous range
+        self.states=list(itertools.product(*[possible_values,possible_costs]*self.model.N)) # all possible states
+        self.actions=list(itertools.product([False,True],repeat=self.model.N))
+        self.dqlearner=DQlearner(self.states,range(len(self.actions)),gamma=gamma,alpha=alpha,n_features=len(self.states[0]))
+        self.act=self.actions[0]
+        # if training:
+        #     self.dqlearner.train(max(1,self.model.model.measurement_fct.n1),self.model.model.measurement_fct.n2)
+
+    def get_decision(self, perceptions):
+        current=self.get_current_state()
+        qvals=self.dqlearner.sess.run(self.dqlearner.q_eval, feed_dict={self.dqlearner.s: [current]})[0]
+        probs=boltzmann(qvals,0.1)
+        self.act=np.random.choice(range(len(self.actions)),p=probs)
+        act=self.actions[self.act]
+        ret=[{"contribution":(a["value"] if act[a["agentID"]] else np.nan),"cost":(a["cost"] if act[a["agentID"]] else np.nan),"privacy":1,"agentID":a["agentID"],"contributed":act[a["agentID"]],"timestep":a["timestep"],"threshold":a["threshold"]} for a in perceptions]
+        return ret
+
+    def feedback(self,perceptions,reward,rew_type="reward"):
+        current=self.get_current_state()
+        ## count only reward of contributors
+        # self.reward=np.nanmean(np.array([r[rew_type] for r in reward])[list(self.act)]) # TODO it could be nan
+        ## use all rewards
+        self.reward=np.mean([r[rew_type] for r in reward])
+        self.dqlearner.learn(current,self.states[0],self.act,self.reward)
+
+    def get_qtable(self):
+        print(self.actions)
+        return self.dqlearner.get_qtable().assign(idx=0)
+
+    def get_qcount(self):
+        return self.dqlearner.get_qcount().assign(idx=0)
+
+    def get_current_state(self):
+        perceptions=self.model.current_state["perception"]
+        pop=list(enumerate([p["agentID"] for p in perceptions])) # indexes at which each agentID is
+        pop=sorted(pop,key=lambda x: x[1]) # sort by agentID
+        state=[[perceptions[i]["value"],perceptions[i]["cost"]] for i,_ in pop] # collect in order of agentID, ascending
+        state=[i for j in state for i in j] # flatten
+        return state
 
 np.random.seed(1)
 tf.set_random_seed(1)
