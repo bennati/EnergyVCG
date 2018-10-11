6e6cc66f73070be2187103a9b8cb943722f7458b
bennati <st.bennati@gmail.com>
2018-10-11 15:57:37 +0200

    Removed stats of actions during logging, as they are cathegorical

--- nego/src/simulation.py
+++ nego/src/simulation.py
@@ -37,15 +37,16 @@ def run_experiment(test,conf,datadir="./"):
                                  evaluation_fct=conf["eval_fct"],
                                  agent_type=NegoAgent)
             model.run(params=params)
+            log=model.log[-1]
             ## save intermediate results
-            log_tot=log_tot+[model.get_log(params)]
+            log_tot=log_tot+[log]
         save_result(datadir,test,{k:v for k,v in params.items() if k!="repetition"},log_tot[-int(conf["reps"]):]) # log the last conf['reps'] iterations (repetitions)
     varnames=[k for k,v in conf["params"].items() if len(v)>1] # keep vars for which there is more than one value
     for varname in varnames:
         print("logging "+varname)
         stats_rew=get_stats(log_tot,"reward",idx=[varname])
         stats_perc=get_stats(log_tot,"perception",idx=[varname],cols=["production","consumption","tariff"])
-        stats_decs=get_stats(log_tot,"decisions",idx=[varname],cols=["action","cost"])
+        stats_decs=get_stats(log_tot,"decisions",idx=[varname],cols=["cost"])
         stats_eval=get_stats(log_tot,"evaluation",idx=[varname],cols=["gini","efficiency",
                                                                       "wealth_distribution",
                                                                       "social_welfare",
@@ -59,5 +60,5 @@ def run_experiment(test,conf,datadir="./"):
             os.makedirs(plotdir)
         plot_trend(stats_rew,varname,os.path.join(plotdir,"./rewards_"+str(test)+"_"+str(varname)+"_nego.pdf"),trends=[t for t in ["reward","produce_avg"] if t !=varname])
         plot_trend(stats_perc,varname,os.path.join(plotdir,"./perceptions_"+str(test)+"_"+str(varname)+"_nego.pdf"),trends=["production","consumption","tariff"])
-        plot_trend(stats_decs,varname,os.path.join(plotdir,"./decisions_"+str(test)+"_"+str(varname)+"_nego.pdf"),trends=["action","cost"])
+        plot_trend(stats_decs,varname,os.path.join(plotdir,"./decisions_"+str(test)+"_"+str(varname)+"_nego.pdf"),trends=["cost"])
         plot_measures_nego(stats_eval,varname,os.path.join(plotdir,"./eval_"+str(test)+"_"+str(varname)+"_nego.pdf"))
